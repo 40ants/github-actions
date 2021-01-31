e9803caf34061f904a183877714745e672c35982
Alexander Artemenko <svetlyak.40wt@gmail.com>
2021-01-31 03:25:18 +0300

    Move to newer cl-anafanfo.

--- local-build.sh
+++ local-build.sh
@@ -4,4 +4,4 @@ export BUILD_DIR=`pwd`
 export BUILDPACK_DIR=$BUILD_DIR/../heroku-buildpack-common-lisp
 export CACHE_DIR=$BUILD_DIR/cache
 
-ros run -L sbcl-bin --asdf --load "$BUILDPACK_DIR/setup/compile.lisp" --quit
+ros run -L ccl-bin --asdf --load "$BUILDPACK_DIR/setup/compile.lisp" --quit
