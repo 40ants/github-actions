e96ae71c435378f7b6ce9e8302903d39ff6ef4b3
Alexander Artemenko <svetlyak.40wt@gmail.com>
2022-07-28 13:15:39 +0300

    Add build.sh

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+NAME=github-actions
+VERSION=0.1.0
+
+export DOCKER_BUILDKIT=1
+
+set -ex
+
+docker build --add-host 'beta.quicklisp.org:13.33.243.6' --progress plain --tag cr.yandex/crp7b1mum9l62quiuuu0/$NAME:$VERSION .
+
+# docker push cr.yandex/crp7b1mum9l62quiuuu0/$NAME:$VERSION
