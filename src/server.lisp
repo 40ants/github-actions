a90f3f137f261000b495387f5912f54394b5b0d7
Alexander Artemenko <svetlyak.40wt@gmail.com>
2022-08-03 10:02:25 +0300

    Use Hunchentoot because Woo server has a bug when processing multiple requests.

--- src/server.lisp
+++ src/server.lisp
@@ -38,7 +38,19 @@
   (start-slynk-if-needed)
   (setf *server*
         (clack:clackup 'app/app::process-request
-                       :server :woo
+                       ;; Clack uses hunchentoot:one-thread-per-connection-taskmaster
+                       ;; for processing multiple requests with hunchentoot server
+                       ;; By default it may create unlimited number of threads and
+                       ;; this may cause DoS.
+                       ;; Thus it is better to put some limits;
+                       :server :hunchentoot
+                       :max-thread-count 32
+                       :max-accept-count 64
+                       ;; Woo has an issue with free worker selection:
+                       ;; https://github.com/fukamachi/woo/issues/100
+                       ;; That is why it is disabled for now.
+                       ;; :server :woo
+                       ;; :worker-num 32
                        :address interface
                        :debug debug
                        :port port))
